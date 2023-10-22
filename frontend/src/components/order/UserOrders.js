38d6dcbe572864fd9b7f44242332f6d76a099d64
it21807794 <sagaratk59@gmail.com>
2023-10-22 17:07:46 +0530

    Invoice Detailing

--- frontend/src/components/order/UserOrders.js
+++ frontend/src/components/order/UserOrders.js
@@ -47,7 +47,7 @@ export default function UserOrders () {
 
         userOrders.forEach(userOrder => {
             data.rows.push({
-                id:  userOrder._id,
+               //id:  userOrder._id,
                 numOfItems: userOrder.orderItems.length,
                 amount: `LKR${userOrder.totalPrice}`,
                 status: userOrder.orderStatus && userOrder.orderStatus.includes('Delivered') ?
@@ -73,6 +73,9 @@ export default function UserOrders () {
                 bordered
                 striped
                 hover
+                entriesOptions={[5, 10, 15, 20]}
+                 noBottomColumns={true} 
+                      
                 data={setOrders()}
             />
         </Fragment>
